e04feadac29f4c3cdc79213102cd0904f3aeb01c
ckmishra2 <chandrakalatiwari2@gmail.com>
2022-09-21 14:51:46 +0530

    Add: Ability to allow player to choose a X letter or O

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,9 +1,42 @@
 package wkshpr;
 
-public class TicTacToe {
+import java.util.Scanner;
 
-	public TicTacToe() {
-		// TODO Auto-generated constructor stub
+public class TicTacToe {
+	static char[] board = new char[10];
+	static char playerLetter;
+	static char computerLetter;
+
+	public static void main(String[] args) {
+		System.out.println("welcome to Tic Tac Toe GAME");
+		board(); // calling the method board in main method
+		chooseLetter();
+	}
+
+	public static void board() {
+		for (int i = 1; i < 10; i++) {
+			board[i] = ' ';
+		}
+
+	}
+
+	public static void chooseLetter() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("please  select X  or O ");
+		char userInput = sc.next().charAt(0); // allowing user to choose character using consol
+		if (userInput == 'X') {
+			playerLetter = userInput;
+			computerLetter = 'O';
+		} else if (userInput == 'O') {
+			playerLetter = userInput;
+			computerLetter = 'X';
+		} else { // if user is entering wrong choice , default letter will be allocated
+			System.out.println("wrong selection ! Now computer will assign letters.");
+			computerLetter = 'X';
+			playerLetter = 'O';
+		}
+		System.out.println("player letter is " + playerLetter);
+		System.out.println("computer letter is " + computerLetter);
 	}
 
 }
